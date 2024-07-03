7c756ad4cc80159cd31927f551d308b6a99f264a
Krishna Sathvik <92758533+KrishnaSathvik@users.noreply.github.com>
2024-07-03 16:23:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,25 @@ const cors = require('cors');
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/travel_planner', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/travel_planner', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 
+// Test route to verify application is running
+app.get('/api/test', (req, res) => {
+  res.send('API is working');
+});
+
 // Routes
 const userRoutes = require('./routes/user');
+const blogRoutes = require('./routes/blog');
 app.use('/api/user', userRoutes);
+app.use('/api/blog', blogRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
